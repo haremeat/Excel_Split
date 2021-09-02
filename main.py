ca3070d61ea8884da98c31a8db122e8e9c846acd
haremeat <dmswjd9341@naver.com>
2021-09-02 16:40:38 +0900

    excel practice

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-chunksize = 10000
+chunksize = 5000
 i = 0
-df = pd.read_excel("D:/python_excel/test.xlsx")
+df = pd.read_excel("D:/python_excel/merck_all.xlsx")
 for chunk in np.array_split(df, len(df) // chunksize):
     chunk.to_excel('./excel/merck_{:02d}.xlsx'.format(i), index=True)
     i += 1
---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from Tools.scripts.dutree import display
+
+df = pd.DataFrame({"A": [1, 4, 7], "B": [2, 5, 8], "C": [3, 6, 9]})
+
+display(df.iloc[0])
+display(df.loc[0])
+display(df.ix[0])
+
+display(df.loc[:, 'A'])
+display(df['A'])
+
+# 특정 row, column을 선택하기
+display(df.ix[0]['A'])
+display(df.loc[0]['B'])
