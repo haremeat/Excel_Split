57565d8083210e34ba948af33d287b5ebd87fa83
haremeat <dmswjd9341@naver.com>
2021-09-27 11:00:31 +0900

    number change

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from Tools.scripts.dutree import display
 
 df = pd.DataFrame({"A": [1, 4, 7], "B": [2, 5, 8], "C": [3, 6, 9]})
 
-display(df.iloc[0])
-display(df.loc[0])
+display(df.iloc[1])
+display(df.loc[1])
 display(df.ix[1])
 
 display(df.loc[:, 'A'])
