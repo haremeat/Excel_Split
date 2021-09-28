e1afc7062712b7879010dbed867bc7637d4971f8
haremeat <dmswjd9341@naver.com>
2021-09-28 14:31:16 +0900

    pandas

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from Tools.scripts.dutree import display
 
 df = pd.DataFrame({"A": [1, 4, 7], "B": [2, 5, 8], "C": [3, 6, 9]})
 
-display(df.iloc[1])
-display(df.loc[1])
-display(df.ix[1])
+display(df.iloc[2])
+display(df.loc[2])
+display(df.ix[2])
 
 display(df.loc[:, 'A'])
 display(df['A'])
